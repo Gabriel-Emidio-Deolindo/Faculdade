505f22397fb2dad4352b72ed9d68f4299d065824
GabrielEmídioDeolindo <gabriel.emidiolindo@gmail.com>
2026-08-06 21:11:09 -0300

    azul e vermelho de acordo com impar ou par

--- Programação Web/Aula-06-08/prog1.js
+++ Programação Web/Aula-06-08/prog1.js
@@ -1,3 +1,12 @@
 var valor;
+var cont;
 valor = prompt('Digite um valor ');
-console.log('o valor atual é '+valor);
+//console.log('o valor atual é '+valor);
+  for(cont=1; cont<=valor; cont++){
+        if(cont % 2 == 0){
+        document.write('<br><p style = "color: blue";>o valor atual é par '+cont);
+        }
+        else{
+        document.write('<br><p style = "color: red";>o valor atual é impar '+cont);
+        }
+}
